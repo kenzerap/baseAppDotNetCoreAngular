7418798239b24b5085d5e36693733b27504b91dd
hoang.nguyen <hoang.nguyen@saigontechnology.com>
2018-07-04 15:41:32 +0700

    create

---
+++ BaseDotNetCoreWithAngular/BaseDotNetCoreWithAngular/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseDotNetCoreWithAngular.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult getMessage()
+        {
+            return Ok(new { message = "hello" });
+        }
+    }
+}
