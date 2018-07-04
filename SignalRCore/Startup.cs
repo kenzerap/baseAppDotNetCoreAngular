33ceea2b0100b405e13452a36efe2b874019cbc3
hoang.nguyen <hoang.nguyen@saigontechnology.com>
2018-07-04 15:14:52 +0700

    create

---
+++ SignalRCore/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SignalRCore
+{
+    public class Startup
+    {
+        private const string DefaultCorsPolicyName = "MyPolicy";
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //Access-Control-Allow-Origin
+            services.AddCors(options => options.AddPolicy(DefaultCorsPolicyName,
+                builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials()));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(DefaultCorsPolicyName);
+
+            //default route: '/api/[controller]'
+            app.UseMvcWithDefaultRoute();
+
+            //configures app to serve the index.html file from /wwwroot
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
